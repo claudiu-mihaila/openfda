66c00dd41c21b62d1364b1534d72aac609b73c93
claudiu-mihaila <klaudiumihaila@gmail.com>
2018-02-17 09:10:08 +0000

    normalised reactions, heatmap

--- src/reporter.py
+++ src/reporter.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 import pandas
 
+from scipy.spatial.distance import euclidean, pdist, squareform
+
 from src.openfda import get_reactionmeddrapt_event_count_by_occurcountry, OpenFDA
 
 
@@ -31,25 +33,35 @@ class Report:
             else:
                 # terms for which no results are returned need to be removed
                 missing_terms.append(r)
-        final_df = pandas.concat(df_append, axis = 1)
+        final_df = pandas.concat(df_append, axis=1)
         final_df.columns = [x for x in self.reaction_counts.term[90:100] if x not in missing_terms]
         print(final_df.shape)
-        return final_df
+        return final_df.fillna(0)
 
     def report_reactionmeddrapt_by_occurcountry(self):
         df = self.get_reactionmeddrapt_by_occurcountry()
 
-        fig = plt.figure()
+        plt.figure()
         df.sum(axis=1).plot(kind="bar", figsize=(30, 6), fontsize=11)
         plt.title("Number of adverse events reported by country")
-        fig.savefig("../output/reactions_by_country.png", dpi=fig.dpi)
+        plt.gcf().savefig("../output/reactions_by_country.png", dpi=100)
 
-        fig = plt.figure()
+        plt.figure()
         df.transpose().describe().loc["count"].sort_values().plot(kind="bar", figsize=(32, 12), fontsize=11)
         plt.title("Number of distinct adverse events reported by country")
-        fig.savefig("../output/distinct_reactions_by_country.png", dpi=fig.dpi)
+        plt.gcf().savefig("../output/distinct_reactions_by_country.png", dpi=100)
 
-        fig = plt.figure()
+        plt.figure()
         df.transpose().describe().loc["count"].plot(kind="hist", figsize=(15, 3), fontsize=11)
         plt.title("Histogram of number of distinct adverse events reported by country")
-        fig.savefig("../output/hist_distinct_reactions_by_country.png", dpi=fig.dpi)
+        plt.gcf().savefig("../output/hist_distinct_reactions_by_country.png", dpi=100)
+
+        plt.figure()
+        df.divide(df.sum(axis=1), axis=0).plot(kind="bar", figsize=(32, 12), fontsize=11, stacked=True)
+        plt.title("Normalised adverse events reported by country")
+        plt.gcf().savefig("../output/normalised_reactions_by_country.png", dpi=100)
+
+        dists = pdist(df.divide(df.sum(axis=1), axis=0), euclidean)
+        similarity_df = pandas.DataFrame(squareform(dists), columns=df.index, index=df.index)
+        plt.imshow(similarity_df, cmap='hot', interpolation='nearest')
+        plt.gcf().savefig("../output/similar_countries_heatmap.png", dpi=100)
