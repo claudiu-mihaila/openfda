cece86f60b066622cb30709ec96e5c4d0c142aa9
claudiu-mihaila <klaudiumihaila@gmail.com>
2018-02-18 15:10:39 +0000

    updated images

--- src/main.py
+++ src/main.py
@@ -5,14 +5,16 @@ from src.reporter import Report
 openfda = OpenFDA()
 
 # fetch some data from openFDA, and save it locally for use
-# events = openfda.load_from_api_counts(get_event_drug_characterization(2, "../data/drugcharacterization:2.json")) # load default search query
-# events = openfda.load_from_api_counts(get_event_count_by_medicinalproduct(2), "../data/count:medicinalproduct.json") # load default search query
-# events = openfda.load_from_api_counts(get_event_count_by_occurcountry(), "../data/count:occurcountry.json") # load default search query
-# events = openfda.load_from_api_counts(get_event_count_by_reactionmeddrapt(), "../data/count:reactionmeddrapt.json") # load default search query
+# events = openfda.load_from_api_counts(get_event_drug_characterization(2, "../data/drugcharacterization:2.json"))
+# events = openfda.load_from_api_counts(get_event_count_by_medicinalproduct(2), "../data/count:medicinalproduct.json")
+# events = openfda.load_from_api_counts(get_event_count_by_occurcountry(), "../data/count:occurcountry.json")
+# events = openfda.load_from_api_counts(get_event_count_by_reactionmeddrapt(), "../data/count:reactionmeddrapt.json")
 
+print("reading stored data...")
 countries = openfda.load_from_file("../data/count:occurcountry.json")
 reactions = openfda.load_from_file("../data/count:reactionmeddrapt.json")
 medicines = openfda.load_from_file("../data/count:medicinalproduct.json")
 
 reporter = Report(countries, reactions, medicines)
-reporter.report_reactionmeddrapt_by_occurcountry()
+print("starting report...")
+reporter.report_reactionmeddrapt_by_occurcountry(cached_data=True, debug=True)
--- src/reporter.py
+++ src/reporter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+import numpy
 import pandas
-
+from matplotlib.ticker import FuncFormatter, MaxNLocator, FixedLocator, IndexLocator
 from scipy.spatial.distance import euclidean, pdist, squareform
 
 from src.openfda import get_reactionmeddrapt_event_count_by_occurcountry, OpenFDA
@@ -15,16 +16,18 @@ class Report:
         self.reaction_counts = pandas.DataFrame(reaction_json)
         self.medicine_counts = pandas.DataFrame(medicine_json)
 
-    def get_reactionmeddrapt_by_occurcountry(self):
-        openfda = OpenFDA()
+    def get_reactionmeddrapt_by_occurcountry(self, debug=False):
+        openfda = OpenFDA(debug=debug)
         df_append = []
         missing_terms = []
-        for r in self.reaction_counts.term[90:100]:
+        for r in self.reaction_counts.term:
             term = "+".join(r.split(" "))
+            # some characters break the API, either raw or encoded/quoted by urllib2
             term = "+".join(term.split("^"))
             term = "+".join(term.split("'"))
             term = "+".join(term.split(","))
-            print(term)
+            if debug:
+                print(term)
             results = openfda.load_from_api_counts(get_reactionmeddrapt_event_count_by_occurcountry(term))
             if len(results) > 0:
                 df = pandas.DataFrame(results)
@@ -34,12 +37,23 @@ class Report:
                 # terms for which no results are returned need to be removed
                 missing_terms.append(r)
         final_df = pandas.concat(df_append, axis=1)
-        final_df.columns = [x for x in self.reaction_counts.term[90:100] if x not in missing_terms]
-        print(final_df.shape)
+        final_df.columns = [x for x in self.reaction_counts.term if x not in missing_terms]
         return final_df.fillna(0)
 
-    def report_reactionmeddrapt_by_occurcountry(self):
-        df = self.get_reactionmeddrapt_by_occurcountry()
+    def report_reactionmeddrapt_by_occurcountry(self, cached_data=False, debug=False):
+        if cached_data:
+            if debug:
+                print("fetching cached data for report...")
+            df = pandas.read_pickle("../data/cached_reactionmeddrapt_by_occurcountry.pkl")
+        else:
+            if debug:
+                print("fetching data for report from API...")
+            df = self.get_reactionmeddrapt_by_occurcountry()
+            df.to_pickle("../data/cached_reactionmeddrapt_by_occurcountry.pkl")
+
+        if debug:
+            print(df.shape)
+            print("reporting")
 
         plt.figure()
         df.sum(axis=1).plot(kind="bar", figsize=(30, 6), fontsize=11)
@@ -56,12 +70,31 @@ class Report:
         plt.title("Histogram of number of distinct adverse events reported by country")
         plt.gcf().savefig("../output/hist_distinct_reactions_by_country.png", dpi=100)
 
+        normalised_df = df.divide(df.sum(axis=1), axis=0)
         plt.figure()
-        df.divide(df.sum(axis=1), axis=0).plot(kind="bar", figsize=(32, 12), fontsize=11, stacked=True)
+        normalised_df.plot(kind="bar", figsize=(32, 12), fontsize=11, stacked=True)
         plt.title("Normalised adverse events reported by country")
         plt.gcf().savefig("../output/normalised_reactions_by_country.png", dpi=100)
 
-        dists = pdist(df.divide(df.sum(axis=1), axis=0), euclidean)
+        dists = pdist(normalised_df, euclidean)
         similarity_df = pandas.DataFrame(squareform(dists), columns=df.index, index=df.index)
+        plt.figure()
+        fig, ax = plt.subplots()
+        fig.set_size_inches(40, 40)
+        ax.xaxis.tick_top()
+        plt.xticks(rotation=70)
+
+        def format_fn(tick_val, tick_pos):
+            if tick_val >= 0 and int(tick_val) in range(len(similarity_df.index)):
+                return similarity_df.index[int(tick_val)]
+            else:
+                return ''
+
+        ax.xaxis.set_ticks(numpy.arange(0, len(similarity_df.index), 1))
+        ax.xaxis.set_major_formatter(FuncFormatter(format_fn))
+        ax.xaxis.set_major_locator(IndexLocator(1, 0))
+        ax.yaxis.set_major_formatter(FuncFormatter(format_fn))
+        ax.yaxis.set_major_locator(IndexLocator(1, 0))
         plt.imshow(similarity_df, cmap='hot', interpolation='nearest')
-        plt.gcf().savefig("../output/similar_countries_heatmap.png", dpi=100)
+        plt.colorbar()
+        plt.gcf().savefig("../output/similar_countries_heatmap.png", dpi=200)
